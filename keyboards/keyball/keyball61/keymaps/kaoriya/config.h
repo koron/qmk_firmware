35641cefa883c5458f5c3e50df063d2699663edb
MURAOKA Taro <koron.kaoriya@gmail.com>
2024-03-21 19:19:32 +0900

    stop to upload SROM

--- keyboards/keyball/keyball61/keymaps/kaoriya/config.h
+++ keyboards/keyball/keyball61/keymaps/kaoriya/config.h
@@ -24,4 +24,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAP_CODE_DELAY 5
 
 // Upload SROM (ID:0x81) to PMW3360 optical sensor.
-#define KEYBALL_PMW3360_UPLOAD_SROM_ID 0x81
+//#define KEYBALL_PMW3360_UPLOAD_SROM_ID 0x81
