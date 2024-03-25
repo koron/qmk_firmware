49dbd5206caf0089ac21538849a338fdbe98e2ca
MURAOKA Taro <koron.kaoriya@gmail.com>
2024-03-26 03:08:47 +0900

    separate mouse layer

--- keyboards/keyball/keyball61/keymaps/kaoriya/config.h
+++ keyboards/keyball/keyball61/keymaps/kaoriya/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAP_CODE_DELAY 5
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 2
+#define AUTO_MOUSE_DEFAULT_LAYER 4
 
 // Upload SROM (ID:0x81) to PMW3360 optical sensor.
 //#define KEYBALL_PMW3360_UPLOAD_SROM_ID 0x81
