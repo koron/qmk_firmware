12bc22872078dc9786ca123509a2e9a4480d2649
MURAOKA Taro <koron.kaoriya@gmail.com>
2024-04-10 22:20:42 +0900

    change path

--- bin/fetch_keyball.sh
+++ bin/fetch_keyball.sh
@@ -2,5 +2,5 @@
 
 set -eu
 
-cp -Rp ../../Yowkees/keyball/qmk_firmware/keyboards/keyball keyboards/
+cp -Rp ../keyball/qmk_firmware/keyboards/keyball keyboards/
 find keyboards/keyball -type f -regex '.*\.\(c\|h\|json\|md\)' | xargs dos2unix -q
